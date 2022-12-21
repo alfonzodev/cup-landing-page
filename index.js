55799fd6683c4008dc9392a81b8feb635a00bb5e
José Alfonzo <alfonzo.dev@gmail.com>
2022-12-21 20:36:32 +0000

    Add JS functionality to hamburguer menu

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.getElementById("header-img");
+const hamburguer = document.querySelector(".hamburguer");
+const navLinks = document.querySelectorAll(".nav-link");
 
+// Change navbar styling on scroll
 document.addEventListener("scroll", (event) => {
   if (window.scrollY >= navbar.offsetHeight) {
     navbar.classList.add("navbar-colored");
@@ -9,4 +12,22 @@ document.addEventListener("scroll", (event) => {
     navbar.classList.remove("navbar-colored");
     logo.src = "images/logo-black.png";
   }
+});
+
+// On Click hamburguer menu icon activate/deactivate menu
+hamburguer.addEventListener("click", () => {
+  if (navbar.classList.contains("active")) {
+    navbar.classList.remove("active");
+  } else {
+    navbar.classList.add("active");
+  }
+});
+
+// On click navlinks deactivate hamburguer menu
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    if (navbar.classList.contains("active")) {
+      navbar.classList.remove("active");
+    }
+  });
 });
